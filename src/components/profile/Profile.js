be5d9e1c4bc3471e5c8fded2a6338e3ea3856b76
Harry MacCode <emarh.harry.code@gmail.com>
2024-01-15 15:14:11 +0100

    Profile App - Search by user profile Logic

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -5,16 +5,32 @@ import { FaTrashAlt } from 'react-icons/fa';
 
 const Profile = () => {
   const [userProfile, setUserProfile] = useState(profiles);
+  const [search, setSearch] = useState('');
 
   const removeProfile = (id) => {
     const newProfileList = userProfile.filter(profile => profile.id !== id);
     setUserProfile(newProfileList);
   }
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  }
+
   return (
     <section className="profile-sec">
         <div className="container">
             <h2 className='--text-light'>Profile App</h2>
-            {userProfile.map(profile => {
+            <div className="--form-control mySearch">
+                <input type="text" placeholder='Enter search term ...' className='--width-100' onChange={handleInputChange} value={search} />
+            </div>
+            {userProfile.filter((value) => {
+                if (search === '') {
+                    return value;
+                } else if(value.name.toLowerCase().includes(search.toLowerCase())) {
+                    return value;
+                }
+            }).map(profile => {
                 return (
                     <div className='profile --card --flex-between' key={profile.id}>
                         <img src={profile.img} alt={profile.img} />
